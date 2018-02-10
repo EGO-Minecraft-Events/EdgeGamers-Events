9f12aa29434a1a09c0e129dc4f9a54bb122c9a41
Austin Siew <glowing.locker@gmail.com>
2018-02-09 22:09:08 -0700

    Changed coordinate files for other libraries

--- lib/vector-pyexpander.py
+++ lib/vector-pyexpander.py
@@ -1,78 +1,233 @@
-from lib.coord import Coord
+#from lib.coord import Coord
 from math import sqrt
 from numbers import Real
+from abc import ABC, abstractmethod
 
-#from coord import Coord
+from coord import Coord
 
 """
 TODO: more doctests and docstrings
 """
 
-class Vector3:
+class Vector(ABC):
+
+    @abstractmethod
+    def copy(self):
+        """Return a copy of this vector
+
+        Returns:
+            A copy of this vector such that the components of
+            the vector are of equal value but do not refer to
+            the same objects.
+        """
+        pass
+
+    @abstractmethod
+    def dot(self, vec):
+        """Return the dot product between this vector and another
+        """
+        pass
+
+    def norm(self):
+        """Return the norm of this vector
+
+        Returns:
+            The length / norm / magnitude of this vector
+        """
+        return sqrt(self.dot(self))
+
+    def normalized(self):
+        """Return a normalized version of this Vector
+
+        Returns:
+            This vector but with a length of 1
+        """
+        return self / self.norm()
+
+    @abstractmethod
+    def __len__(self):
+        """Return the number of components (x,y,z,w,...) in this vector, also called the size
+
+        Returns:
+            The size of this vector
+        """
+        pass
+
+    @abstractmethod
+    def __iter__(self):
+        """Return an iterable of the components of this vector
+        """
+
+    @abstractmethod
+    def __getitem__(self, key):
+        """Allow retriving components of this vector via indexing in 
+        a list-like fashion and with 'x','y','z','w', ... ,etc.
+
+        Raises:
+            IndexError when out of bounds or vector does not contain the key
+        """
+        pass
+
+    @abstractmethod
+    def __setitem__(self, key, value):
+        """Allow modification of components of this vector via indexing in 
+        a list-like fashion and with 'x','y','z','w', ... ,etc.
+
+        Raises:
+            IndexError when out of bounds or vector does not contain the key
+        """
+        pass
+
+    @abstractmethod
+    def __repr__(self):
+        """Return a string that looks like a valid Python expression to
+        recreate this vector
+        """
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        """Return a string that makes this vector readable
+        """
+        pass
+
+    @abstractmethod
+    def simple_str(self):
+        """Return a simplified string version of this vector
+
+        Returns:
+            A space separated string of coordinates from this vector
+        """
+        pass
+
+    """Arthimetic comparisons
+    """
+
+    @abstractmethod
+    def __eq__(self, other):
+        """If each component of this vector equals (in value, not necessarily reference)
+        each component of the other vector, return True
+        """
+        pass
+
+    @abstractmethod
+    def __ne__(self, other):
+        """If any of the components in this vector differ from the same corresponding component
+        of the other vector, return True
+        """
+        pass
+
+    """Matrix multiplication
+    """
+
+    @abstractmethod
+    def __matmul__(self, other):
+        """Vector * Matrix
+        """
+        pass
+
+    """Arithmetic operations are performed component-wise
+    """
+
+$py(ops = {"add__":"+", "sub__":"-", "mul__":"*", "truediv__":"/", "floordiv__":"//", "mod__":"%", "pow__":"**", "lshift__":"<<", "rshift__":">>", "and__":"&", "xor__":"^", "or__":"|"})
+$for(name, symbol in ops.items())
+    @abstractmethod
+    def __$(name)(self, other):
+        pass
+
+    def __r$(name)(self, other):
+        pass
+
+    def __i$(name)(self, other):
+        """Should return NotImplemented if any of the vector
+        components in self refer to the same object.
+        """
+        pass
+$endfor
+
+    @abstractmethod
+    def __divmod__(self, other):
+        return
+
+    @abstractmethod
+    def __rdivmod__(self, other):
+        return
+
+$py(unary_ops = {"__neg__":"-", "__pos__":"+", "__invert__":"~"})
+$for(name, symbol in unary_ops.items())
+    @abstractmethod
+    def $(name)(self):
+        """Apply the unary arithmetic operation to each component
+        """
+        pass
+$endfor
+
+    @abstractmethod
+    def __abs__(self):
+        """Take the absolute value of each component
+        """
+        pass
+
+    @abstractmethod
+    def __round__(self, n=0):
+        """Round each component to the n'th decimal place
+        """
+        pass
+
+
+
+
+class Vector3(Vector):
     """A class for storing and manipulating three coordinates
 
     Attributes:
         x (Coord): x-coordinate
         y (Coord): y-coordinate
         z (Coord): z-coordinate
-
-    Examples:
-        >>> a = Vector3('1', '2', '3')
-        >>> a
-        Vector3('1','2','3')
-
-        >>> a = a.zyx
-        >>> a
-        Vector3('3','2','1')
-
-        >>> a.xz += 1
-        >>> a
-        Vector3('4','2','2')
-
-        >>> a.xy = a.yx
-        >>> a
-        Vector3('2','4','2')
-
-        >>> for c in a: c += 1
-        >>> a
-        Vector3('3','5','3')
-
-        >>> a[0] = 4
-        >>> a
-        Vector3('4','5','3')
-
-        >>> a = a.yxz
-        >>> a
-        Vector3('5','4','3')
-
-        >>> a.simple_str()
-        '5 4 3'
     """
 
-    def __init__(self, x=0, y=None, z=None):
+$for(i in 'xyz')
+    @property
+    def $(i)(self):
+        return self._$(i)
+
+    @$(i).setter
+    def $(i)(self, value):
+        self._$(i) = Coord(value)
+$endfor
+
+    def __init__(self, x=0, y=None, z=None, by_ref=False):
         """Create a new vector
 
         Args:
-            x (optional): x-coordinate. Has a default value of 0.
-            y (optional): y-coordinate. If None, z must also be None, and y will inherit its value from x. If not None, z must also not be None.
-            z (optional): z-coordinate. If None, y must also be None, and z will inherit its value from x. If not None, y must also not be None.
+            x (optional): x-coordinate. Has a default value of 0. Both y and z will inherit this value if both are None or unspecified
+            y (optional): y-coordinate. Inherits the value of x if both y and z are None or unspecified. 0 if z is specified and y is not.
+            z (optional): z-coordinate. Inherits the value of x if both y and z are None or unspecified. 0 if y is specified and z is not.
+            by_ref (optional): Whether to assign self.x, self.y, self.z by reference or value when x, y, and/or z are Coord objects.
+                Exception is when x is the only value specified, in which case x y z will be assigned by value regardless of by_ref.
 
         Examples:
-            Valid uses: Vector3(), Vector3(x), Vector3(x, y, z)
             >>> Vector3()
             Vector3('0','0','0')
+            >>> Vector3('~')
+            Vector3('~','~','~')
+            >>> Vector3('^')
+            Vector3('^','^','^')
             >>> Vector3(1.0)
             Vector3('1.0','1.0','1.0')
-            >>> Vector3(1.0, 2.0, 3.0)
-            Vector3('1.0','2.0','3.0')
+            >>> Vector3(1, 2.0, -3.0)
+            Vector3('1','2.0','-3.0')
             >>> Vector3('~5')
             Vector3('~5','~5','~5')
+            >>> Vector3('~5', '^6', 2)
+            Vector3('~5','^6','2')
+            >>> Vector3('^-2', '~3.0')
+            Vector3('^-2','~3.0','0')
+            >>> Vector3('^-2', z='~3.0')
+            Vector3('^-2','0','~3.0')
         """
-        if y is not None and z is None:
-            raise TypeError("Invalid usage. Valid uses: Vector3(), Vector3(x), Vector3(x, y, z). More details in docstring.")
-        if y is None and z is not None:
-            raise TypeError("Invalid usage. Valid uses: Vector3(), Vector3(x), Vector3(x, y, z). More details in docstring.")
 
+        # If both y and z are None, inherit the value of x
         if y is None and z is None:
             if isinstance(x, Coord):
                 self.x = x.copy()
@@ -84,61 +239,61 @@ class Vector3:
                 self.z = Coord(x)
             return
 
-        if isinstance(x, Coord):
+        # Otherwise, set the y or z to 0
+        if y is None:
+            y = 0
+        if z is None:
+            z = 0
+
+        if not by_ref:
             self.x = x
+            self.y = y
+            self.z = z
+            return
+
+        # Set the values of self._x, self._y, self._z
+        if isinstance(x, Coord):
+            self._x = x
         else:
-            self.x = Coord(x)
+            self.x = x
 
-        if y is not None:
-            if isinstance(y, Coord):
-                self.y = y
-            else:
-                self.y = Coord(y)
-
-        if z is not None:
-            if isinstance(z, Coord):
-                self.z = z
-            else:
-                self.z = Coord(z)
-
-$for(i in 'xyz')
-    @property
-    def $(i)(self):
-        return self._$(i)
-
-    @$(i).setter
-    def $(i)(self, value):
-        if isinstance(value, Coord):
-            self._$(i) = value.copy()
-        elif isinstance(value, (str, Real)):
-            self._$(i) = Coord(value)
+        if isinstance(y, Coord):
+            self._y = y
         else:
-            raise TypeError("Type 'Coord' can only be assigned to 'int', 'float', 'long', any subclass of numbers.Real, 'str', or 'Coord'")
-$endfor
+            self.y = y
 
-    def copy(self):
-        """Return a copy of this vector
+        if isinstance(z, Coord):
+            self._z = z
+        else:
+            self.z = z
+
+    def cross(self, vec3):
+        """Return the cross product
+
+        Args:
+            vec3 (Vector3): A Vector3
 
         Returns:
-            Vector3(self.x.copy(), self.y.copy(), self.z.copy())
+            A Vector3 that is the cross product of this vector and vec3
+        
+        Examples:
+            >>> vec = Vector3()
+            >>> vec_copy = vec.copy()
         """
-        return Vector3(self.x, self.y, self.z)
-
-    def norm(self):
-        """Return the norm of this vector
-
-        Returns:
-            The length / norm / magnitude of this vector
-        """
-        return sqrt(self @ self)
-
-    def cross(self, vector3):
         if not isinstance(vector3, Vector3):
-            raise TypeError("A cross product is only applicable to Vector3's")
+            raise TypeError("A cross product is only applicable to a Vector3")
         x = self.y * vector3.z - self.z * vector3.y
         y = self.z * vector3.x - self.x * vector3.z
         z = self.x * vector3.y - self.y * vector3.x
         return Vector3(x, y, z)
+
+    def dot(self, vec):
+        if not isinstance(vec, Vector3):
+            raise TypeError("Vector sizes must match")
+        return self.x * vec.x + self.y * vec.y + self.z * vec.z
+
+    def copy(self):
+        return Vector3(self.x.copy(), self.y.copy(), self.z.copy())
 
     def __len__(self):
         return 3
@@ -147,9 +302,6 @@ $endfor
         yield self.x
         yield self.y
         yield self.z
-
-    def normalized(self):
-        return self / self.norm()
 
     def __getitem__(self, key):
         keys = "xyz"
@@ -186,11 +338,6 @@ $endfor
         return "({},{},{})".format(str(self.x), str(self.y), str(self.z))
 
     def simple_str(self):
-        """Return a space separated string of coordinates
-
-        Returns:
-            "{} {} {}".format(str(self.x), str(self.y), str(self.z))
-        """
         return "{} {} {}".format(str(self.x), str(self.y), str(self.z))
 
 $for(i in 'xyz')
@@ -269,11 +416,7 @@ $endfor
             return NotImplemented
 
     def __matmul__(self, other):
-        # TODO: Matrix multiplication with matrices
-        if isinstance(other, Vector3):
-            return (self.x * other.x + self.y * other.y + self.z * other.z).value
-        else:
-            return NotImplemented
+        return NotImplemented
 
     """Arithmetic operations are performed component-wise
     """
@@ -353,15 +496,26 @@ class Vector2:
         y (Coord): y-coordinate
     """
 
-    def __init__(self, x=0, y=None):
+$for(i in 'xy')
+    @property
+    def $(i)(self):
+        return self._$(i)
+
+    @$(i).setter
+    def $(i)(self, value):
+        self._$(i) = Coord(value)
+$endfor
+
+    def __init__(self, x=0, y=None, by_ref=False):
         """Create a new vector
 
         Args:
             x (optional): x-coordinate. Has a default value of 0
             y (optional): y-coordinate. If None, will inherit x's value
+            by_ref (optional): Whether to assign self.x, self.y by reference or value when x, and/or y are Coord objects.
+                Exception is when x is the only value specified, in which case x y will be assigned by value regardless of by_ref.
 
         Examples:
-            Valid uses: Vector2(), Vector2(x), Vector2(x, y)
             >>> Vector2()
             Vector2('0','0')
             >>> Vector2(1.0)
@@ -380,47 +534,27 @@ class Vector2:
                 self.y = Coord(x)
             return
 
-        if isinstance(x, Coord):
+        if not by_ref:
             self.x = x
+            self.y = y
+
+        if isinstance(x, Coord):
+            self._x = x
         else:
-            self.x = Coord(x)
+            self.x = x
 
-        if y is not None:
-            if isinstance(y, Coord):
-                self.y = y
-            else:
-                self.y = Coord(y)
-
-$for(i in 'xy')
-    @property
-    def $(i)(self):
-        return self._$(i)
-
-    @$(i).setter
-    def $(i)(self, value):
-        if isinstance(value, Coord):
-            self._$(i) = value.copy()
-        elif isinstance(value, (str, Real)):
-            self._$(i) = Coord(value)
+        if isinstance(y, Coord):
+            self._y = y
         else:
-            raise TypeError("Type 'Coord' can only be assigned to 'int', 'float', 'long', any subclass of numbers.Real, 'str', or 'Coord'")
-$endfor
+            self.y = y
+    
+    def dot(self, vec):
+        if not isinstance(vec, Vector2):
+            raise TypeError("Vector sizes must match")
+        return self.x * vec.x + self.y * vec.y
 
     def copy(self):
-        """Return a copy of this vector
-
-        Returns:
-            Vector2(self.x.copy(), self.y.copy())
-        """
-        return Vector2(self.x, self.y)
-
-    def norm(self):
-        """Return the norm of this vector
-
-        Returns:
-            The length / norm / magnitude of this vector
-        """
-        return sqrt(self @ self)
+        return Vector2(self.x.copy(), self.y.copy())
 
     def __len__(self):
         return 2
@@ -428,9 +562,6 @@ $endfor
     def __iter__(self):
         yield self.x
         yield self.y
-
-    def normalized(self):
-        return self / self.norm()
 
     def __getitem__(self, key):
         keys = "xy"
@@ -463,11 +594,6 @@ $endfor
         return "({},{})".format(str(self.x), str(self.y))
 
     def simple_str(self):
-        """Return a space separated string of coordinates
-
-        Returns:
-            "{} {}".format(str(self.x), str(self.y))
-        """
         return "{} {}".format(str(self.x), str(self.y))
 
 $for(i in 'xy')
@@ -528,11 +654,7 @@ $endfor
             return NotImplemented
 
     def __matmul__(self, other):
-        # TODO: Matrix multiplication with matrices
-        if isinstance(other, Vector2):
-            return (self.x * other.x + self.y * other.y).value
-        else:
-            return NotImplemented
+        return NotImplemented
 
     """Arithmetic operations are performed component-wise
     """
