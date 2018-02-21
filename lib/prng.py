60f8d6975f0c1a835ec6721ff00ceb4afdbc7fe8
Icohedron <icohedron@gmail.com>
2018-02-20 19:41:59 -0700

    Change prng initialization

--- lib/prng.py
+++ lib/prng.py
@@ -1,15 +1,13 @@
 from lib.scoreboard import Objective, OBJECTIVES
-from lib.floo_network import Event
 from lib.container import Container
 from lib.const_ints import CONST_INTS
 
 class PRNG(Container):
-    def __init__(self, event, objective_name, lower, upper=None):
+    def __init__(self, objective_name, lower, upper=None):
         """
         Create a new PRNG object for generating pseudo-random numbers
 
         Args:
-            event (Event): event object for self-referencing mcfunctions
             objective_name (str): objective name for storing scoreboard values
             lower (int): the minimum value (inclusive) for the resulting pseudo-random number
             upper (int, optional): the maximum value (non-inclusive) for the resulting pseudo-random number
@@ -20,22 +18,20 @@ class PRNG(Container):
         if lower and not upper:
             lower, upper = 0, lower
         
-        self.event = event
         self.objective_name = objective_name
         self.objective = Objective(objective_name, display_name="Pseudo-RNG")
         self.objective.add_const("&Multiplier", 1103515245)
         self.objective.add_const("&Increment", 12345)
         self.objective.add_const("&Offset", lower)
         self.objective.add_const("&Modulus", upper - lower)
-        self.objective.add_const("&Seed", 0) # This isn't a constant, it will change! Why does the method have to be called "add_const"!?
+        self.objective.add_const("&Seed", 0)
         OBJECTIVES.add(self.objective)
 
     def cmd_init(self):
         self.cmd_queue.put('summon area_effect_cloud ~ ~ ~ {Tags:["PRNG","True"]}')
-        self.cmd_queue.put('summon area_effect_cloud ~ ~ ~ {Tags:["PRNG","False"]}')
-        self.cmd_queue.put('summon area_effect_cloud ~ ~ ~ {Tags:["PRNGIterator"]}')
-        self.cmd_queue.put("@e[PRNGIterator] {} = 1".format(self.objective_name))
-        self.cmd_queue.put(self.event.cmd_func("prng_init"))
+        self.cmd_queue.put('summon area_effect_cloud ~ ~ ~ {Tags:["PRNG"]}')
+        for i in range(31): # Construct a random positive value for the seed by generating random bits. Seed is stored in-game as a 32-bit signed integer, so we generate 31 random bits
+            self.cmd_queue.put("@r[type=area_effect_cloud,PRNG] @s[True] &Seed {} + {}".format(self.objective_name, 1 << i))
         return self._cmd_output()
     
     def cmd_next_seed(self):
